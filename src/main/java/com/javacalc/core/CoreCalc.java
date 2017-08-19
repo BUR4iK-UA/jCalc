8c6a440e7ddc71498ad5ebaa244f5dc6e0ed31de
BUR4iK-UA <burko0121998@gmail.com>
2017-08-19 16:04:46 +0300

    Update

--- src/main/java/com/javacalc/core/CoreCalc.java
+++ src/main/java/com/javacalc/core/CoreCalc.java
@@ -1,4 +1,18 @@
 package main.java.com.javacalc.core;
 
 public class CoreCalc {
+    public static String[] arrayTokens(String s){
+        String[] numb  = s.split("[\\*\\+/-]");
+        String[] symbol = s.split("(\\(?\\d+\\.?\\d+\\)?)|\\d");
+        String[] res = new String[numb.length+symbol.length-1];
+        for (int i =0,j = 0; i < numb.length;){
+            res[j] = numb[i];
+            i++;
+            j++;
+            if(i< symbol.length)res[j] = symbol[i];
+            j++;
+        }
+
+        return res;
+    }
 }
--- src/main/java/com/javacalc/runner/RunnerCmd.java
+++ src/main/java/com/javacalc/runner/RunnerCmd.java
@@ -1,8 +1,19 @@
 package main.java.com.javacalc.runner;
 
+import java.util.Arrays;
+
+import main.java.com.javacalc.core.CoreCalc;
+
 public class RunnerCmd {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        String in = "2+2+2";
+        String s= "12*3-4+342/2.54+(232-232*2323)";
+        System.out.println(Arrays.toString(s.split("(\\*|-|\\+|/)")));
+        System.out.println(Arrays.toString(s.split("[\\*\\+/-]")));
+        System.out.println(Arrays.toString(CoreCalc.arrayTokens(s)));
+
+
+        System.out.println(s);
     }
 
 }
