3e0b5af6270006188612b89d0e68fcb5db00ce20
BUR4iK-UA <burko0121998@gmail.com>
2017-08-20 17:59:00 +0300

    Update CoreCalc

--- src/main/java/com/javacalc/core/CoreCalc.java
+++ src/main/java/com/javacalc/core/CoreCalc.java
@@ -1,9 +1,9 @@
 package main.java.com.javacalc.core;
 
 public class CoreCalc {
-    public static String[] arrayTokens(String s){
-        String[] numb  = s.split("[\\*\\+/-]");
-        String[] symbol = s.split("(\\(?\\d+\\.?\\d+\\)?)|\\d");
+    public static String[] arrayTokens(String expression){
+        String[] numb  = expression.split("[\\*\\+/-]");
+        String[] symbol = expression.split("(\\(?\\d+\\.?\\d+\\)?)|\\d");
         String[] res = new String[numb.length+symbol.length-1];
         for (int i =0,j = 0; i < numb.length;){
             res[j] = numb[i];
